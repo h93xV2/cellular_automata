ebc470d180396585e9c5a3f8015482967c789538
Lucas <h93xV2@users.noreply.github.com>
2021-05-18 20:29:27 -0700

    Cell copier fix

--- src/main/java/cellular_automata/cells/CellArrayCopier.java
+++ src/main/java/cellular_automata/cells/CellArrayCopier.java
@@ -8,7 +8,7 @@ public class CellArrayCopier {
     final Cell[][] copyCells = new Cell[cells.length][cells[0].length];
 
     for (var x = 0; x < cells.length; x++) {
-      for (var y = 0; y < cells.length; y++) {
+      for (var y = 0; y < cells[0].length; y++) {
         if (cells[x][y] != null) {
           copyCells[x][y] = (Cell) cells[x][y].clone();
         }
