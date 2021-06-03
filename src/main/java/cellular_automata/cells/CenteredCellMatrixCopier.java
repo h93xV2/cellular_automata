7b621a8936eec543f26e1ddd8b0d00622d8317b6
Lucas <h93xV2@users.noreply.github.com>
2021-06-03 00:13:50 -0700

    Refactor

--- src/main/java/cellular_automata/cells/CenteredCellMatrixCopier.java
+++ src/main/java/cellular_automata/cells/CenteredCellMatrixCopier.java
@@ -4,19 +4,21 @@ public class CenteredCellMatrixCopier implements CellMatrixCopier {
 
   @Override
   public void copyCellStates(Cell[][] source, CellMatrix destination) {
-    final int xOffset = (destination.getWidth() - source.length) / 2;
-    final int yOffset = (destination.getHeight() - source[0].length) / 2;
+    destination.forEach((x, y) -> destination.getCell(x, y).setState(CellState.DEAD));
 
-    for (var x = 0; x < source.length; x++) {
-      for (var y = 0; y < source[0].length; y++) {
+    final int sourceWidth = source.length;
+    final int sourceHeight = source[0].length;
+    final int xOffset = (destination.getWidth() - sourceWidth) / 2;
+    final int yOffset = (destination.getHeight() - sourceHeight) / 2;
+
+    for (var x = 0; x < sourceWidth; x++) {
+      for (var y = 0; y < sourceHeight; y++) {
         final int xDestination = x + xOffset;
         final int yDestination = y + yOffset;
 
         if (0 <= xDestination && xDestination < destination.getWidth() && 0 <= yDestination
             && yDestination < destination.getHeight()) {
-          if (source[x][y] == null) {
-            destination.getCell(xDestination, yDestination).setState(CellState.DEAD);
-          } else {
+          if (source[x][y] != null) {
             destination.getCell(xDestination, yDestination).setState(source[x][y].getState());
           }
         }
