1aeaca0ad84640eced18e47286e41f248117a259
h93xV2 <h93xV2@users.noreply.github.com>
2025-12-13 00:12:47 -0800

    Remove suppress warnings

--- src/main/java/cellular_automata/filemanagement/parsers/CellStateLineDetectedException.java
+++ src/main/java/cellular_automata/filemanagement/parsers/CellStateLineDetectedException.java
@@ -1,6 +1,5 @@
 package cellular_automata.filemanagement.parsers;
 
-@SuppressWarnings("serial")
 class CellStateLineDetectedException extends RuntimeException {
   public CellStateLineDetectedException() {
     super();
--- src/main/java/cellular_automata/filemanagement/parsers/UnknownHeaderAttributeException.java
+++ src/main/java/cellular_automata/filemanagement/parsers/UnknownHeaderAttributeException.java
@@ -1,6 +1,5 @@
 package cellular_automata.filemanagement.parsers;
 
-@SuppressWarnings("serial")
 public class UnknownHeaderAttributeException extends RuntimeException {
   public UnknownHeaderAttributeException() {
     super();
